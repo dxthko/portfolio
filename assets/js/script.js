9f28f81137bdc05bb955ac50a10b07c5936eb538
dxthko <h.sp97@hotmail.com>
2022-07-01 20:47:04 -0300

    added script for light theme and back to top button

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,23 @@ function openMenu() {
 function closeMenu() {
   document.body.classList.remove('menu-expanded')
 }
+
+const toTop = () => window.scrollTo({ top: 0, behavior: 'smooth' })
+
+const checkbox = document.getElementById('checkbox')
+checkbox.addEventListener('change', () => {
+  document.body.classList.toggle('light')
+})
+
+/* scroll reveal lib */
+ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700
+}).reveal(`
+  .header,
+  .about,
+  .contact,
+  .projects,
+  .services
+  `)
